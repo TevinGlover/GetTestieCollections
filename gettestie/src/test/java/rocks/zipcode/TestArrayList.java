2207ed24de90f2971111ac16443ad818cc0e80d2
Tevin Glover <tevwork.tg@gmail.com>
2023-12-01 07:27:40 -0500

    ArrayList Start

--- gettestie/src/test/java/rocks/zipcode/TestArrayList.java
+++ gettestie/src/test/java/rocks/zipcode/TestArrayList.java
@@ -1,5 +1,28 @@
 package rocks.zipcode;
 
+import org.junit.Before;
+
+import java.util.ArrayList;
+
 public class TestArrayList {
     // Tips from The Leuth use https://www.w3schools.com and https://www.baeldung.com for the types of Collections.
+    String a;
+    String as;
+    String ae;
+    String ap;
+
+    String amax;
+    @Before
+    public void Setup(){
+        ArrayList<String> String  = new ArrayList<>();
+         a = "a";
+         as = "as";
+         ae = "ate";
+         ap= "append";
+         amax= "amax";
+
+    }
+
+
 }
+
