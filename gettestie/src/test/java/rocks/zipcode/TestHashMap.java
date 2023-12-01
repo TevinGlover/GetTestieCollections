8f2ada192bd94b184cf6875014774e8ee8003443
Tevin Glover <tevwork.tg@gmail.com>
2023-12-01 10:46:24 -0500

    Queue

--- gettestie/src/test/java/rocks/zipcode/TestHashMap.java
+++ gettestie/src/test/java/rocks/zipcode/TestHashMap.java
@@ -1,5 +1,82 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
 public class TestHashMap {
     // Tips from The Leuth use https://www.w3schools.com and https://www.baeldung.com for the types of Collections.
+
+        String westI;
+        String southI;
+        String northI;
+        String eastI;
+        Integer nine;
+        Integer four;
+        Integer  two;
+        Integer six;
+        HashMap<String,Integer> interState;
+
+
+        @Before
+        public void Setup() {
+            interState = new HashMap<>();
+            eastI = "a";
+            southI = "as";
+            northI= "ate";
+            westI = "append";
+            nine = 95;
+            four = 400;
+            two =273;
+            six = 69;
+
+        }
+        @Test
+        public void testHashMap(){
+            Assert.assertNotNull(interState);
+        }
+        @Test
+        public void testHashMapSize(){
+            Assert.assertNotNull(interState);
+            Assert.assertEquals(0, interState.size());
+        }
+        @Test
+        public void testHashMapPut(){
+            Assert.assertNotNull(interState);
+            interState.put(southI,six);
+
+            Assert.assertEquals(1, interState.size());
+        }
+
+        @Test
+        public void testHashMapRemove(){
+            Assert.assertNotNull(interState);
+            interState.put(southI,six);
+            interState.remove(southI,six);
+
+            Assert.assertEquals(0, interState.size());
+        }
+
+        @Test
+        public void testHashMapContains(){
+            Assert.assertNotNull(interState);
+            interState.put(southI,six);
+            interState.put(westI,nine);
+            interState.put(southI,six);
+            interState.remove(southI,six);
+
+            Assert.assertTrue( interState.containsKey(westI));
+        }
+        @Test
+        public void testHashMapGet(){
+            Assert.assertNotNull(interState);
+            interState.put(southI,six);
+            interState.put(westI,nine);
+            interState.put(eastI,four);
+            Integer expect = four;
+
+            Assert.assertEquals( expect, interState.get(eastI));
+        }
 }
--- gettestie/src/test/java/rocks/zipcode/TestHashSet.java
+++ gettestie/src/test/java/rocks/zipcode/TestHashSet.java
@@ -1,28 +1,66 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class TestHashSet {
     // Tips from The Leuth use https://www.w3schools.com and https://www.baeldung.com for the types of Collections.
 
-    String a;
-    String as;
-    String ate;
-    String ap;
-    ArrayList<String> waxOn;
-    String amax;
+    String westI;
+    String southI;
+    String northI;
+    String eastI;
+
+    HashSet<String> interState;
+
 
     @Before
     public void Setup() {
-        waxOn = new ArrayList<>();
-        a = "a";
-        as = "as";
-        ate = "ate";
-        ap = "append";
-        amax = "amax";
+        interState = new HashSet<>();
+        eastI = "a";
+        southI = "as";
+        northI= "ate";
+        westI = "append";
 
+    }
+@Test
+    public void testHashSet(){
+    Assert.assertNotNull(interState);
+}
+    @Test
+    public void testHashMapSize(){
+        Assert.assertNotNull(interState);
+        Assert.assertEquals(0, interState.size());
+    }
+    @Test
+    public void testHashSetadd(){
+        Assert.assertNotNull(interState);
+        interState.add(southI);
 
+        Assert.assertEquals(1, interState.size());
+    }
+
+    @Test
+    public void testHashSetRemove(){
+        Assert.assertNotNull(interState);
+        interState.add(southI);
+        interState.remove(southI);
+
+        Assert.assertEquals(0, interState.size());
+    }
+
+    @Test
+    public void testHashSetContains(){
+        Assert.assertNotNull(interState);
+        interState.add(southI);
+        interState.add(westI);
+        interState.add(southI);
+        interState.remove(southI);
+
+       Assert.assertTrue( interState.contains(westI));
     }
 }
